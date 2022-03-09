2905c4424fe6e99a0240ae2525ed70583d816f5d
Christof Lemke <christoflemke@github.com>
2022-03-09 20:38:08 +0100

    simplify configuration

--- script/configure.sh
+++ script/configure.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -e
+set -eu
 
-echo 'Please enter the credentials for influx db (default admin:admin)'
-read -p 'Influx Username: ' INFLUX_USER
-read -sp 'Influx Password: ' INFLUX_PASSWORD
-echo -e '\n'
+source .master.env
+
 if [ -z "$INFLUX_USER" ]; then
   INFLUX_USER='admin'
 fi
@@ -13,10 +11,6 @@ if [ -z "$INFLUX_PASSWORD" ]; then
   INFLUX_PASSWORD='admin'
 fi
 
-echo 'Please enter the credentials for grafana (default: admin:admin)'
-read -p 'Grafana Username: ' GRAFANA_USER
-read -sp 'Grafana Password: ' GRAFANA_PASSWORD
-echo -e '\n'
 if [ -z "$GRAFANA_USER" ]; then
   GRAFANA_USER='admin'
 fi
@@ -24,14 +18,8 @@ if [ -z "$GRAFANA_PASSWORD" ]; then
   GRAFANA_PASSWORD='admin'
 fi
 
-read -p 'Client cert location:' CLIENT_CERT_LOCATION
 CLIENT_CERT_LOCATION=$(readlink -f $(eval echo $CLIENT_CERT_LOCATION))
-
-read -p 'Client key location:' CLIENT_KEY_LOCATION
 CLIENT_KEY_LOCATION=$(readlink -f $(eval echo $CLIENT_KEY_LOCATION))
-
-read -p 'Bosch bridge IP address:' BOSCH_IP
-
 CLIENT_CERT=$(base64 -w0 "$CLIENT_CERT_LOCATION")
 CLIENT_KEY=$(base64 -w0 "$CLIENT_KEY_LOCATION")
 
@@ -42,7 +30,9 @@ GRAFANA_USER=${GRAFANA_USER}
 GRAFANA_PASSWORD=${GRAFANA_PASSWORD}
 EOF
 
-cat <<EOF > config/default.json
+createConfig() {
+  INFLUX_HOST=$1
+  cat <<EOF
 {
   "bosch": {
     "clientCert": "${CLIENT_CERT}",
@@ -50,11 +40,20 @@ cat <<EOF > config/default.json
     "baseUrl": "https://${BOSCH_IP}:8444"
   },
   "influx": {
-    "host": "localhost",
+    "host": "${INFLUX_HOST}",
     "username": "${INFLUX_USER}",
     "password": "${INFLUX_PASSWORD}",
     "port": 8086,
     "database": "db0"
-  }
+  },
+  "airthings": {
+      "username": "${AIRTHINGS_USER}",
+      "password": "${AIRTHINGS_PASSWORD}",
+      "intervalSeconds": 600
+    }
 }
 EOF
+}
+
+createConfig localhost > config/default.json
+createConfig influxdb > config/docker.json
