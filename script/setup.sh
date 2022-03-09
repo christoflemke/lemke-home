7062ae093022deb9357fa220de8f227dab9a5a29
Christof Lemke <christoflemke@github.com>
2022-03-09 07:50:37 +0100

    setup scripts

---
+++ script/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)
+cd ..
+
+if [ ! -f .env ]; then
+  echo 'missing .env, run ./script/configure.sh first'
+fi
+
+if [ ! -f config/default.json ]; then
+  echo 'missing config/default.json, run ./script/configure.sh first'
+fi
+
+docker-compose up -d
