c52bb1662f04d4b9bc0f094b90c3ed4539ed40ea
Christof Lemke <christoflemke@github.com>
2022-03-15 07:26:29 +0100

    ignore deploy script

--- script/sync-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-cd ..
-
-ssh lemkepi bash -c 'cd /home/lemke/code/lemke-home/ && git pull'
-rsync -r --exclude node_modules/ config lemkepi:/home/lemke/code/lemke-home/config
-ssh lemkepi bash -c '/home/lemke/code/lemke-home/script/setup.sh'
