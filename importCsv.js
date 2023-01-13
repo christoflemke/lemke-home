fa128e6365fd8c4ee7848bbec6481da5324a00b2
Christof Lemke <christoflemke@github.com>
2023-01-13 10:50:40 +0100

    incremental import of fjernvame data

--- importCsv.js
+++ importCsv.js
@@ -8,9 +8,6 @@ async function readAll() {
   for await (const dirent of dir) {
     await csv.reader(path.join(uploadPath, dirent.name))
   }
-  // await csv.reader('Kredslob2Solstien_230110_074559.csv')
-  // await csv.reader('Kredslob2Solstien_230110_111200.csv')
-  // await csv.reader('Kredslob2Solstien_230111_113730.csv')
   console.log('done')
 }
 readAll()
